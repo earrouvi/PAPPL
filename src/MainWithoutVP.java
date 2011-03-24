4a66d9328853d6a7242218afb337f98c59f1f69c
Elsa <renard.mail@gmail.com>
2011-03-24 17:26:45 +0100

    elsa27

--- src/MainWithoutVP.java
+++ src/MainWithoutVP.java
@@ -1,6 +1,9 @@
 import OutlineComputation.ExtractFrontOutlineFunction;
 import OutlineComputation.FinalOutlinePoints;
 import Scissors.ScissorsOutlineFunction;
+import java.awt.*;
+import java.util.*;
+import straightenUp.*;
 
 
 public class MainWithoutVP {
@@ -18,6 +21,20 @@ public class MainWithoutVP {
 
 		ExtractFrontOutlineFunction efof = new ExtractFrontOutlineFunction(sof.getVl().getPoints(), filepath);
 		FinalOutlinePoints outlinePoints = efof.computeFrontOutline();
+		
+		// enter coordinates of the corners :
+		Point p1, p2, p3, p4;
+		p1 = new Point(0,0);
+		p2 = new Point(0,500);
+		p3 = new Point(250,500);
+		p4 = new Point(250,0);
+		ArrayList<Point> endPoints = new ArrayList<Point>();
+		endPoints.add(p1); endPoints.add(p2); endPoints.add(p3); endPoints.add(p4); 
+		
+		Homography h = new Homography(outlinePoints, endPoints);
+		
+		ImageStraightening im = new ImageStraightening(filepath);
+		im.straightenUp(h);
 	}
 
 }
